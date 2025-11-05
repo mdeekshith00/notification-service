d077116ff4e00bc6dea14ed9c2763836ac5e6e14
mdeekshith00 <mdeekshith000@gmail.com>
2025-11-05 11:30:38 +0530

    impl consumer

---
+++ src/main/java/com/notification/Notification_Service/service/KafkaConsumer.java
@@ -0,0 +1,33 @@
+package com.notification.Notification_Service.service;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.common.vo.UserEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaConsumer {
+
+	private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @KafkaListener(topics = "user-events", groupId = "notification-service-group")
+    public void consume(String message) {
+        try {
+            UserEvent event = objectMapper.readValue(message, UserEvent.class);
+
+            System.out.println("🔥 Received event from Kafka");
+            System.out.println("EventType: " + event.getEventType());
+            System.out.println("User: " + event.getUserId());
+            System.out.println("Email: " + event.getEmail());
+            System.out.println("Message: " + event.getMessage());
+
+            // TODO: send email / SMS here
+        } catch (Exception e) {
+            System.out.println("Error parsing message");
+        }
+    }
+}
